7945f11afcaa5061d9df282463f73561aec8f2f4
aaryansinha16 <aaryansinha16@gmail.com>
2023-02-16 02:24:39 +0530

    layout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,28 @@
+import { Flex } from '@chakra-ui/react'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import About from './components/About/About'
+import ChatList from './components/ChatList/ChatList'
+import MainChat from './components/MainChat/MainChat'
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Navbar />
+      <Flex
+        justifyContent='space-between'
+        alignItems='center'
+        border='1px solid red'
+        w='100%'
+        // m='auto'
+        h='200px'
+      >
+        <ChatList />
+        <MainChat />
+        <About />
+      </Flex>
     </div>
   )
 }
---
+++ frontend/src/components/ChatList/ChatList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ChatList = () => {
+  return (
+    <div>
+      ChatList
+    </div>
+  )
+}
+
+export default ChatList
